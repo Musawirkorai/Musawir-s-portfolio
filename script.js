aa4267c099803e8212d53261a127a55de74d1688
Musawirkorai <musawirkorai@gmail.com>
2025-09-24 17:25:57 +0500

    aded blog section

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+const blogCards = document.querySelectorAll(".blog-card");
+
+blogCards.forEach(card => {
+  card.addEventListener("click", () => {
+    // If the clicked blog is already active → close it
+    if (card.classList.contains("active")) {
+      card.classList.remove("active");
+    } else {
+      // Close all blogs first
+      blogCards.forEach(c => c.classList.remove("active"));
+      // Open clicked blog
+      card.classList.add("active");
+    }
+  });
+});
